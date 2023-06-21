10cab502924e4c26811a1902979b1259f31a7164
Blake Ferm <blakeferm@gmail.com>
2023-06-21 17:27:08 -0400

    add custom fonts

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,6 +1,10 @@
 import { useUser } from '@auth0/nextjs-auth0/client';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Logo } from '../Logo';
+Logo;
 
 export default function AppLayout({ children }) {
   const { user } = useUser();
@@ -8,10 +12,18 @@ export default function AppLayout({ children }) {
   return (
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
       <div className="flex flex-col text-white overflow-hidden">
-        <div className="text-black align-center bg-slate-800">
-          <div className="">Logo</div>
-          <div className="">CTA button</div>
-          <div className="">Tokens</div>
+        <div className="align-center bg-slate-800 px-2">
+          <Logo />
+          <Link
+            href="/post/new"
+            className="bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block"
+          >
+            New Post
+          </Link>
+          <Link href="/token-topup" className="block mt-2 text-center">
+            <FontAwesomeIcon icon={faCoins} className="text-yellow-500" />
+            <span className="pl-1">0 Tokens available</span>
+          </Link>
         </div>
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
           List of posts
