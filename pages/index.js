f3f501f953262c80b0f658f9bedb9cbd11308500
Blake Ferm <blakeferm@gmail.com>
2023-06-21 16:19:59 -0400

    update again

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,3 @@
-import { useUser } from '@auth0/nextjs-auth0/client';
-import Image from 'next/image';
-import Link from 'next/link';
-
 export default function Home() {
-  const { user } = useUser();
-  console.log(user);
-
-  return (
-    <div>
-      {!!user ? (
-        <>
-          <div>All user info</div>
-          <Image src={user.picture} alt={user.name} height={50} width={50} />
-          <div>{user.email}</div>
-          <Link href="/api/auth/logout">Logout</Link>
-        </>
-      ) : (
-        <Link href="/api/auth/login">Login</Link>
-      )}
-    </div>
-  );
+  return <div></div>;
 }
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,7 +1,22 @@
-export default function NewPost() {
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import AppLayout from '../../components/AppLayout';
+
+export default function NewPost({ test }) {
   return (
     <div>
       <h1> NewPost Page </h1>
     </div>
   );
 }
+
+NewPost.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
+
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {
+      test: 'this is a test',
+    },
+  };
+});
