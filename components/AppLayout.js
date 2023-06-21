705f82a0d56b4e66870c56e0e686dc00414f73e1
Blake Ferm <blakeferm@gmail.com>
2023-06-21 16:44:29 -0400

    style user info in sidebar

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -16,18 +16,24 @@ export default function AppLayout({ children }) {
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
           List of posts
         </div>
-        <div className="bg-cyan-800">
+        <div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
           {!!user ? (
             <>
-              <div>All user info</div>
-              <Image
-                src={user.picture}
-                alt={user.name}
-                height={50}
-                width={50}
-              />
-              <div>{user.email}</div>
-              <Link href="/api/auth/logout">Logout</Link>
+              <div className="min-width-[50px]">
+                <Image
+                  src={user.picture}
+                  alt={user.name}
+                  height={50}
+                  width={50}
+                  className="rounded-full"
+                />
+              </div>
+              <div className="flex-1">
+                <div className="font-bold">{user.email}</div>
+                <Link className="text-sm" href="/api/auth/logout">
+                  Logout
+                </Link>
+              </div>
             </>
           ) : (
             <Link href="/api/auth/login">Login</Link>
