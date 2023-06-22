7d93cc8a6ca0ed5239eb71f950bee2d888d874ef
Blake Ferm <blakeferm@gmail.com>
2023-06-22 16:09:43 -0400

    wire up posts in mongodb

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -1,6 +1,22 @@
 import { Configuration, OpenAIApi } from 'openai';
+import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
+import clientPromise from '../../lib/mongodb';
 
-export default async function handler(req, res) {
+export default withApiAuthRequired(async function handler(req, res) {
+  const { user } = await getSession(req, res);
+
+  const client = await clientPromise;
+  const db = client.db('BlogStandard');
+
+  const userProfile = await db.collection('users').findOne({
+    auth0Id: user.sub,
+  });
+
+  if (!userProfile?.availableTokens) {
+    res.status(403);
+    return;
+  }
+
   const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
@@ -80,6 +96,27 @@ export default async function handler(req, res) {
   const metaDescription =
     metaDescriptionResponse.data.choices[0]?.message?.content || '';
 
+  await db.collection('users').updateOne(
+    {
+      auth0Id: user.sub,
+    },
+    {
+      $inc: {
+        availableTokens: -1,
+      },
+    }
+  );
+
+  const post = await db.collection('posts').insertOne({
+    postContent,
+    title,
+    metaDescription,
+    topic,
+    keywords,
+    userId: userProfile._id, // foreign key from mongodb
+    created: new Date(),
+  });
+
   res.status(200).json({
     post: {
       postContent,
@@ -87,4 +124,4 @@ export default async function handler(req, res) {
       metaDescription,
     },
   });
-}
+});
