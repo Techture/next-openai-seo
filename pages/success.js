c2db1b519ff277dd3d745f0f09953900b8195314
Blake Ferm <blakeferm@gmail.com>
2023-07-04 10:51:31 -0400

    update success page

--- pages/success.js
+++ pages/success.js
@@ -6,9 +6,8 @@ export default function Success() {
   return (
     <div className="h-full flex items-center justify-center bg-gradient-to-b from-slate-100 to-slate-300">
       <div className="w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200">
-        <h1 className="text-center">
-          Thanks for your purchase. Now go enjoy creating optimized blog posts.
-        </h1>
+        <h3 className="text-center">Thanks for your purchase.</h3>
+        <p>Now go enjoy creating a few optimized blog posts.</p>
       </div>
     </div>
   );
