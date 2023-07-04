925090d115c712956594e21d789e77b766ff289b
Blake Ferm <blakeferm@gmail.com>
2023-07-04 11:38:35 -0400

    update success page

--- pages/success.js
+++ pages/success.js
@@ -4,10 +4,10 @@ import { getAppProps } from '../utils/getAppProps';
 
 export default function Success() {
   return (
-    <div className="h-full flex items-center justify-center bg-gradient-to-b from-slate-100 to-slate-300">
-      <div className="flex flex-col items-center justify-center w-full max-w-screen-sm bg-slate-100 p-7 rounded-md shadow-xl border border-slate-200 shadow-slate-200">
+    <div className="h-full flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center h-screen w-full bg-slate-100 p-7 rounded-md shadow-xl border border-slate-200 shadow-slate-200">
         <h3 className="my-0 text-center">Thanks for your purchase.</h3>
-        <p className="mt-2">Enjoy creating a few optimized blog posts.</p>
+        <p className="mt-2 text-center">Enjoy creating optimized blog posts.</p>
       </div>
     </div>
   );
